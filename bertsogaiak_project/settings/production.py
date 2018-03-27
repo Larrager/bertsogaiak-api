42c503cb3e187cbb5d7b6f52b206d07ee528cb14
Sergio Peña <sergiop79@gmail.com>
2018-03-27 14:50:29 +0200

    Cors origin allow all

--- bertsogaiak_project/settings/production.py
+++ bertsogaiak_project/settings/production.py
@@ -7,3 +7,5 @@ ALLOWED_HOSTS = ['bertsogaiak.herokuapp.com']
 DATABASES['default'] = dj_database_url.parse(os.environ.get('DATABASE_URL'))
 
 STATIC_URL = 'https://bertsogaiak.herokuapp.com/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
